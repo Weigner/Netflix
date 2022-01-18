0a764e2d45f871d99211fc45b6e59aac7dbb8921
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-01-18 17:37:50 -0300

    Implementando o Adapter e o ViewHolder

--- app/src/main/java/br/com/weigner/netflix/MainActivity.kt
+++ app/src/main/java/br/com/weigner/netflix/MainActivity.kt
@@ -2,10 +2,40 @@ package br.com.weigner.netflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.weigner.netflix.model.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+private class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
+
+private class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
+
+    private lateinit var movies: List<Movie>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.movie_item, parent, false)
+
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        var movie = movies.get(position)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
