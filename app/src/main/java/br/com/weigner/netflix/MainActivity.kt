d68edb0afcf4e25253e34ccbae34b835afefb845
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-01-19 20:32:05 -0300

    Implementando listagem do RecyclerView com dados mocados

--- app/src/main/java/br/com/weigner/netflix/MainActivity.kt
+++ app/src/main/java/br/com/weigner/netflix/MainActivity.kt
@@ -5,23 +5,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.weigner.netflix.model.Movie
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainAdapter: MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view_main)
+
+        val movies = mutableListOf<Movie>()
+        /*for (movie in movies) {
+            var movie = Movie()
+            movie.setCoverUrl("abc")
+            movies.add(movie)
+        }*/
+        var i = 0
+        while (i < 30) {
+            var movie = Movie()
+            movie.setCoverUrl("abc$i")
+            movies.add(movie)
+            i++
+        }
+
+        mainAdapter = MainAdapter(movies)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = mainAdapter
     }
 }
 
 private class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    var textViewUrl: TextView = itemView.findViewById<TextView>(R.id.text_view_url)
+
 }
 
-private class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
+private class MainAdapter(movies: MutableList<Movie>) : RecyclerView.Adapter<MovieViewHolder>() {
 
-    private lateinit var movies: List<Movie>
+    private var movies: List<Movie> = movies
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -32,10 +60,11 @@ private class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         var movie = movies.get(position)
+        holder.textViewUrl.text = movie.getCoverUrl()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return movies.size
     }
 
 }
