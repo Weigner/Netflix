f5ff62cfbaa7cf3c571c24264bfd68faa0585bbd
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-01-20 21:22:29 -0300

    Criando uma listagem de categorias com uma listagem de filmes dentro

--- app/src/main/java/br/com/weigner/netflix/MainActivity.kt
+++ app/src/main/java/br/com/weigner/netflix/MainActivity.kt
@@ -1,7 +1,10 @@
 package br.com.weigner.netflix
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import br.com.weigner.netflix.model.Category
 import br.com.weigner.netflix.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -22,23 +26,68 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view_main)
 
-        val movies = mutableListOf<Movie>()
+        val categories = mutableListOf<Category>()
+
+        var j = 0
+        while (j < 10) {
+            var category = Category()
+            category.setName("cat $j")
+            categories.add(category)
+            j++
+
+            val movies = mutableListOf<Movie>()
+            var i = 0
+            while (i < 30) {
+                var movie = Movie()
+                movie.setCoverUrl(R.drawable.movie)
+                movies.add(movie)
+                i++
+            }
+            category.setMovies(movies)
+            categories.add(category)
+        }
         /*for (movie in movies) {
             var movie = Movie()
             movie.setCoverUrl("abc")
             movies.add(movie)
         }*/
-        var i = 0
-        while (i < 30) {
-            var movie = Movie()
-            movie.setCoverUrl(R.drawable.movie)
-            movies.add(movie)
-            i++
-        }
 
-        mainAdapter = MainAdapter(movies)
-        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        mainAdapter = MainAdapter(categories)
+        recyclerView.layoutManager = LinearLayoutManager(baseContext, RecyclerView.VERTICAL, false)
         recyclerView.adapter = mainAdapter
+    }
+}
+
+private class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val textTitle = itemView.findViewById<TextView>(R.id.text_view_title)
+    val recyclerViewMovie = itemView.findViewById<RecyclerView>(R.id.recycler_view_movie)
+
+}
+
+private class MainAdapter(categories: MutableList<Category>) :
+    RecyclerView.Adapter<CategoryViewHolder>() {
+
+    private var categories: List<Category> = categories
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_item, parent, false)
+
+        return CategoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        var category = categories.get(position)
+        holder.textTitle.text = category.getName()
+        holder.recyclerViewMovie.adapter =
+            MovieAdapter(category.getMovies() as MutableList<Movie>)
+        holder.recyclerViewMovie.layoutManager =
+            LinearLayoutManager(null, RecyclerView.HORIZONTAL, false)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
     }
 }
 
@@ -48,7 +97,8 @@ private class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
 
 }
 
-private class MainAdapter(movies: MutableList<Movie>) : RecyclerView.Adapter<MovieViewHolder>() {
+private class MovieAdapter(movies: MutableList<Movie>) :
+    RecyclerView.Adapter<MovieViewHolder>() {
 
     private var movies: List<Movie> = movies
 
@@ -67,5 +117,4 @@ private class MainAdapter(movies: MutableList<Movie>) : RecyclerView.Adapter<Mov
     override fun getItemCount(): Int {
         return movies.size
     }
-
 }
--- app/src/main/java/br/com/weigner/netflix/model/Category.kt
+++ app/src/main/java/br/com/weigner/netflix/model/Category.kt
@@ -5,4 +5,20 @@ class Category {
     private var name = ""
     private lateinit var movies: List<Movie>
 
+    fun getName(): String {
+        return name
+    }
+
+    fun setName(name: String) {
+        this.name = name
+    }
+
+    fun getMovies(): List<Movie> {
+        return movies
+    }
+
+    fun setMovies(movies: List<Movie>) {
+        this.movies = movies
+    }
+
 }
