27096b7d4d01b26afbfae9cc4522537627f03711
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-01-22 15:29:30 -0300

    Criando MovieActivity.kt e colocando como activity principal

---
+++ app/src/main/java/br/com/weigner/netflix/MovieActivity.kt
@@ -0,0 +1,11 @@
+package br.com.weigner.netflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
