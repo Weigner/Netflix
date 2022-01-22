53320672dc0dfe5f6998432fc8c0e9c1c880fc2d
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-01-22 16:18:53 -0300

    Criando toolbar

--- app/src/main/java/br/com/weigner/netflix/MovieActivity.kt
+++ app/src/main/java/br/com/weigner/netflix/MovieActivity.kt
@@ -2,10 +2,21 @@ package br.com.weigner.netflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        if (supportActionBar != null){
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        }
+
+
     }
 }
